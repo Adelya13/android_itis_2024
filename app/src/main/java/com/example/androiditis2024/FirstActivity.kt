3f7b849b4322f5544a0e340e019c53981a47f247
Adelya Valisheva <amvalisheva@avito.ru>
2024-12-01 16:48:39 +0300

    fixed main

--- app/src/main/java/com/example/androiditis2024/FirstActivity.kt
+++ app/src/main/java/com/example/androiditis2024/FirstActivity.kt
@@ -12,7 +12,6 @@ import com.example.androiditis2024.databinding.FirstActivityBinding
 class FirstActivity: AppCompatActivity() {
 
     private var binding: FirstActivityBinding? = null
-    var value = 0
 
     @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,83 +21,10 @@ class FirstActivity: AppCompatActivity() {
         binding = FirstActivityBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-
-        savedInstanceState?.let{
-            value = it.getInt(VALUE_ARGS)
-            Log.e("ACTIVITY_VALUE", value.toString())
-            binding?.tvValue?.text = value.toString()
-        }
-
-
-        binding?.let{
-            with(it){
-                button1.setOnClickListener{
-                    value++
-                    tvValue.text = value.toString()
-                }
-                button2.setOnClickListener{
-                   SecondActivity.start(this@FirstActivity, value)
-                }
-                button3.setOnClickListener{
-                    val webIntent = Uri.parse("https://www.android.com").let { webpage ->
-                            Intent(Intent.ACTION_VIEW, webpage)
-                    }
-
-                    val chooserIntent = Intent.createChooser(
-                        webIntent,
-                        "Chooser"
-                    )
-
-                    if (intent.resolveActivity(packageManager) != null) {
-                        startActivity(chooserIntent)
-                    }
-                }
-            }
-        }
-
-        Log.e("ACTIVITY_123","onCreate")
-
-
-    }
-
-
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(VALUE_ARGS, value)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        Log.e("ACTIVITY_123","onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.e("ACTIVITY_123","onResume")
-
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.e("ACTIVITY_123","onPause")
-
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.e("ACTIVITY_123","onStop")
-
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("ACTIVITY_123","onDestroy")
-
+        binding = null
     }
-
-    companion object {
-         const val VALUE_ARGS = "VALUE_ARG"
-    }
-
 }
